378c742f335a893f1c824f65a654536c20ee38c4
MackKaputo <mackkaputo@gmail.com>
2020-11-11 22:22:12 +0200

    styled the forms for resolved and pending

--- reporting/jedsreport/static/jedsreport/pending.js
+++ reporting/jedsreport/static/jedsreport/pending.js
@@ -2,6 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#pending').style.display = 'none';
     document.querySelector('#pending-expand').onclick = function(){
         document.querySelector('#pending').style.display = 'block';
+        document.querySelector('#resolved-expand').style.display = 'none';
+
     }
+    document.querySelector('#resolved').style.display = 'none';
+    document.querySelector('#resolved-expand').onclick = function(){
+        document.querySelector('#resolved').style.display = 'block';
+        document.querySelector('#pending-expand').style.display = 'none';
+    }
+    
     
 })
